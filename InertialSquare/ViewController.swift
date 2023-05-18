b2818ec99917387dae4a44fb3f9bee102487c93c
Vladislav Densiov <denisov076@gmail.com>
2023-05-18 12:12:56 +0300

    Initial Commit

---
+++ InertialSquare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InertialSquare
+//
+//  Created by Vladislav Denisov on 18.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
